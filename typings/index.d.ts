1963c84f286a07b2eb8abd5ef782b9859d2a8878
Swad <SwadicalRag@users.noreply.github.com>
2016-06-16 21:54:45 +0930

    Update gitignore

---
+++ typings/index.d.ts
@@ -0,0 +1,6 @@
+/// <reference path="globals/node/index.d.ts" />
+/// <reference path="modules/big-integer/index.d.ts" />
+/// <reference path="modules/cheerio/index.d.ts" />
+/// <reference path="modules/es6-promise/index.d.ts" />
+/// <reference path="modules/form-data/index.d.ts" />
+/// <reference path="modules/request/index.d.ts" />
